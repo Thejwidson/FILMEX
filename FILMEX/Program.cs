06f56994c81d5e804d63205dcedd6774051b5130
Thejwidson <97634046+Thejwidson@users.noreply.github.com>
2024-03-13 21:15:53 +0100

    Komentarz w Program.cs obczajcie pls

--- FILMEX/Program.cs
+++ FILMEX/Program.cs
@@ -3,13 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using FILMEX.Models;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using FILMEX.Models.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 // Add services to the container.
@@ -36,5 +37,28 @@ app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Nie wiem czemu nie moge sie zalogowac na tego admina jak jest w bazie normalnie, moze ktos ogarnie help pls
+using(var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    string email = "Admin@admin123.com";
+    string pwd = "Admin@admin123.com";
+
+    if(await userManager.FindByEmailAsync(email) == null)
+    {
+        var adminUser = new User();
+        adminUser.UserName = "Admin";
+        adminUser.Email = email;
+        adminUser.EmailConfirmed = true;
+        
+
+        await userManager.CreateAsync(adminUser,pwd);
+
+        await userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+
+}
 
 app.Run();
