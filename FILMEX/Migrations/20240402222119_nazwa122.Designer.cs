2eb7f511df6270ee0e31c3b8f352110631909610
Jacob <75269577+Triadziuch@users.noreply.github.com>
2024-04-03 01:34:37 +0200

    backup

--- FILMEX/Migrations/20240402222119_nazwa122.Designer.cs
+++ FILMEX/Migrations/20240402222119_nazwa122.Designer.cs
@@ -4,6 +4,7 @@ using FILMEX.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FILMEX.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240402222119_nazwa122")]
+    partial class nazwa122
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FILMEX/Migrations/20240402222119_nazwa122.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FILMEX.Migrations
+{
+    /// <inheritdoc />
+    public partial class nazwa122 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Comment_ParentId",
+                table: "Comment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comment_ParentId",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Comment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "Comment",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_ParentId",
+                table: "Comment",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Comment_ParentId",
+                table: "Comment",
+                column: "ParentId",
+                principalTable: "Comment",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
