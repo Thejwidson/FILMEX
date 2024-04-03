2393e351869a7c941a2f7ff7e7cce06fab3c9d98
Thejwidson <97634046+Thejwidson@users.noreply.github.com>
2024-04-03 20:34:33 +0200

    Admin v0.1 + kosmetyka jakas drobna

---
+++ FILMEX/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using FILMEX.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FILMEX.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ILogger<AdminController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
