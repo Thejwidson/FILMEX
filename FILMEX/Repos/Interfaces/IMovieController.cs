a3812ca022667d0d47a859e6085627fe26db002d
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2024-04-20 16:50:39 +0200

    Repos

--- FILMEX/Repos/Interfaces/IMovieController.cs
+++ FILMEX/Repos/Interfaces/IMovieController.cs
@@ -4,6 +4,27 @@ namespace FILMEX.Repos.Interfaces
 {
     public interface IMovieController
     {
+        //Movie
         Task<List<Movie>> GetAllMoviesAsync();
+        Task<Movie> GetMoviesWithCommentsAsync(int? id);
+        Task<Movie> GetMoviesWithReviewsAsync(int? id);
+        Task<Movie> FindMoviesAsync(int? id);
+        void AddMovie(Movie movie);
+        void RemoveMovie(Movie movie);
+        void UpdateMovie(Movie movie);
+
+        // User
+        Task<User> FindUserAsync(string? id);
+
+        // ReviewsMovie
+        Task<ReviewMovie> FindReviewByMovieIdAndUserIdAsync(int movieId, string userId);
+        public void AddReview(Movie movie, ReviewMovie review);
+        public void UpdateReview(ReviewMovie review);
+
+        // Comment
+        Task<Comment> FindCommentByIdAsync(int commentId);
+        public void LoadCommentRelations(Comment comment);
+        public void AddComment(Movie movie, Comment comment);
+        Task RemoveComment(Comment comment);
     }
 }
--- FILMEX/Repos/Repositories/MovieRepository.cs
+++ FILMEX/Repos/Repositories/MovieRepository.cs
@@ -2,6 +2,7 @@
 using FILMEX.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
+using System.Linq;
 
 namespace FILMEX.Repos.Repositories
 {
@@ -10,6 +11,7 @@ namespace FILMEX.Repos.Repositories
         private readonly ApplicationDbContext _context;
         private readonly IWebHostEnvironment _webHostEnvironemt;
 
+        // Movie
         public MovieRepository(ApplicationDbContext context, IWebHostEnvironment webHostEnvironemt)
         {
             _context = context;
@@ -19,6 +21,87 @@ namespace FILMEX.Repos.Repositories
         public async Task<List<Movie>> GetAllMoviesAsync()
         {
             return await _context.Movies.ToListAsync();
+        }
+
+        public async Task<Movie> GetMoviesWithCommentsAsync(int? id)
+        {
+            return await _context.Movies.Include(m => m.Comments).ThenInclude(c => c.Author).FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<Movie> GetMoviesWithReviewsAsync(int? id)
+        {
+            return await _context.Movies.Include(m => m.Reviews).FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<Movie> FindMoviesAsync(int? id)
+        {
+            return await _context.Movies.FindAsync(id);
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            _context.Add(movie);
+            _context.SaveChangesAsync();
+        }
+
+        public void RemoveMovie(Movie movie)
+        {
+            _context.Remove(movie); //_context.Movies.Remove(movie);
+            _context.SaveChangesAsync();
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            _context.Update(movie);
+            _context.SaveChangesAsync();
+        }
+
+        // User
+        public async Task<User> FindUserAsync(string? userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        // ReviewsMovie
+        public async Task<ReviewMovie> FindReviewByMovieIdAndUserIdAsync(int movieId, string userId)
+        {
+            return await _context.ReviewsMovie.FirstOrDefaultAsync(r => r.Movie.Id == movieId && r.User.Id == userId);
+        }
+
+        public void AddReview(Movie movie, ReviewMovie review)
+        {
+            movie.Reviews.Add(review);
+            _context.ReviewsMovie.Add(review);
+            _context.SaveChangesAsync();
+        }
+
+        public void UpdateReview(ReviewMovie review)
+        {
+            _context.ReviewsMovie.Update(review);
+            _context.SaveChangesAsync();
+        }
+
+        // Comment
+        public async Task<Comment> FindCommentByIdAsync(int commentId)
+        {
+            return await _context.Comments.FirstOrDefaultAsync(c => c.Id == commentId);
+        }
+
+        public void LoadCommentRelations(Comment comment)
+        {
+            _context.Entry(comment).Reference(c => c.Author).Load();
+            _context.Entry(comment).Reference(c => c.Movie).Load();
+        }
+        public void AddComment(Movie movie, Comment comment)
+        {
+            movie.Comments.Add(comment);
+            _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveComment(Comment comment)
+        {
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
         }
     }
 }
