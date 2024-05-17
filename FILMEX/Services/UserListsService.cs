cd010c2b2e61e3f7ec6f5d669009cffcb6ef57bf
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2024-05-17 03:38:06 +0200

    mastering mvc

---
+++ FILMEX/Services/UserListsService.cs
@@ -0,0 +1,49 @@
+﻿using FILMEX.Models.Entities;
+using FILMEX.Models.ViewModels;
+using FILMEX.Repos;
+using FILMEX.Repos.Interfaces;
+using FILMEX.Services.Interfaces;
+using FILMEX.Views.UserLists;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FILMEX.Services
+{
+    public class UserListsService : IUserListsService
+    {
+        private readonly IUserListsController _userListsController;
+
+        public UserListsService(UserListsRepository userListsController)
+        {
+            _userListsController = userListsController;
+        }
+
+        public async Task<HomeViewModel> GetUserMSListsHome(string userId) 
+        {
+            var userMovies = await _userListsController.FindUserWithMovies(userId);
+            var userSeries = await _userListsController.FindUserWithSeries(userId);
+
+            var viewModel = new HomeViewModel
+            {
+                Movies = userMovies?.MoviesToWatch.ToList() ?? new List<Movie>(),
+                Series = userSeries?.SeriesToWatch.ToList() ?? new List<Series>()
+            };
+
+            return viewModel;
+        }
+
+        public async Task<ToWatchViewModel> GetUserMSLists(string userId)
+        {
+            var userMovies = await _userListsController.FindUserWithMovies(userId);
+            var userSeries = await _userListsController.FindUserWithSeries(userId);
+
+            var viewModel = new ToWatchViewModel
+            {
+                Movies = userMovies?.MoviesToWatch.ToList() ?? new List<Movie>(),
+                Series = userSeries?.SeriesToWatch.ToList() ?? new List<Series>()
+            };
+
+            return viewModel;
+        }
+
+    }
+}
---
+++ FILMEX/Views/UserLists/ToWatchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FILMEX.Models.Entities;
+
+namespace FILMEX.Views.UserLists
+{
+    public class ToWatchViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public List<Series> Series { get; set; }
+    }
+}
