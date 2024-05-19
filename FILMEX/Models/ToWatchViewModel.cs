08417ab769be954aadfaef1be33996671c72d76c
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2024-05-19 13:29:06 +0200

    towatchviewmodel added

---
+++ FILMEX/Models/ToWatchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FILMEX.Models.Entities;
+
+namespace FILMEX.Models
+{
+    public class ToWatchViewModel
+    {
+        public List<Entities.Movie> Movies { get; set; }
+        public List<Series> Series { get; set; }
+    }
+}
--- FILMEX/Services/Interfaces/IUserListsService.cs
+++ FILMEX/Services/Interfaces/IUserListsService.cs
@@ -1,6 +1,11 @@
-﻿namespace FILMEX.Services.Interfaces
+﻿using FILMEX.Models;
+using FILMEX.Models.ViewModels;
+
+namespace FILMEX.Services.Interfaces
 {
     public interface IUserListsService
     {
+        Task<HomeViewModel> GetUserMSListsHome(string userId);
+        Task<ToWatchViewModel> GetUserMSLists(string userId);
     }
 }
